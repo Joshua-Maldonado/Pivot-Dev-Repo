cbb77e24a7ec64e369c6672c52520f63d7ccee12
Joshua Maldonado <jemdeveloping@gmail.com>
2025-12-16 19:35:52 -0500

    Changed tone of message

--- extensions/pivot-dev/src/Checkout.jsx
+++ extensions/pivot-dev/src/Checkout.jsx
@@ -1,6 +1,7 @@
 import '@shopify/ui-extensions/preact';
 import {render} from "preact";
-import {useAppMetafields} from "@shopify/ui-extensions/checkout/preact"
+import { useEffect, useState } from "preact/hooks";
+import {useAppMetafields,useApi,useCartLineTarget} from "@shopify/ui-extensions/checkout/preact"
 
 
 export default function() {
@@ -9,8 +10,10 @@ export default function() {
 
 function Extension() {
   // Get the product ID from the cart line item
+ 
   const {merchandise: {product: {id: productId}}} = shopify.lines.value[0];
-  console.log('Product ID:', productId);
+  
+ 
 
   // Use the merchant-defined metafield for shipping delay instructions and map it to a cart line
   const [productMetafield] = useAppMetafields( {
@@ -20,11 +23,12 @@ function Extension() {
   }).filter(
     (entry) =>  productId.endsWith(entry.target.id)
   );
-  console.log('Product Metafield:', productMetafield);
+
+ 
   // Render the shipping delay instructions if applicable
   if (productMetafield?.metafield?.value) {
     return (
-        <s-text>
+        <s-text tone="warning">
           {productMetafield?.metafield?.value}
         </s-text>
       );
@@ -32,3 +36,5 @@ function Extension() {
   
   return null;
 }
+
+
